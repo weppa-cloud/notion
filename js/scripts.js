833e5f0e9fe637d5c1704ec60cdf4730787bb31b
Mauricio <mauroalternativo519@gmail.com>
2024-09-01 21:32:57 -0500

    Add enviar data con imagen, Add notificacion de envío de data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 let productos = []; // Variable global para almacenar los productos
 let eventData ={};
@@ -15,8 +15,6 @@ function isJSONValid(str) {
 
 // Escuchar eventos del objeto Window
 window.addEventListener("message", function (event) {
-    console.log('Received event data:', event.data);
-
     if (!isJSONValid(event.data)) {
         console.error('Invalid JSON:', event.data);
         return;
@@ -24,11 +22,6 @@ window.addEventListener("message", function (event) {
 
     // Procesar los datos del evento
     eventData = JSON.parse(event.data);
-
-    // Mostrar mensaje de carga
-    // document.getElementById('loading').style.display = 'flex';
-    // document.getElementById('resumen').style.display = 'none';
-
 
 
     // Enviar los datos a n8n y esperar la respuesta
@@ -41,28 +34,14 @@ window.addEventListener("message", function (event) {
     })
     .then(response => response.json())
     .then(n8nResponse => {
-        console.log('Received response from n8n:', n8nResponse);
-
         // Verifica si el campo result contiene el array de objetos
         productos = n8nResponse || [];
         mostrarProductos(productos); // Mostrar todos los productos inicialmente
-        
-        
-
-        // Aquí puedes procesar la respuesta de n8n y modificar los datos si es necesario
-        // const chatwootData = {
-        //     ...eventData, 
-        //     reply: n8nResponse.reply || ''
-        // };
     })
     .catch((error) => {
-        console.error('Error sending data to n8n:', error);
         document.getElementById('loading').style.display = 'none';
     });
-
-    
 });
-
 
 
 
@@ -81,11 +60,10 @@ function mostrarProductos(productos) {
         productos.forEach(producto => {
             const name = producto.name || 'Sin título';
             const description = producto.property_descripci_n || 'Sin descripción';
-            const price = producto.property_tarifa_en_doble || '';
-            // const imageBase64 = producto.image_128;
             const imageUrl = producto.property_url_foto_destacada || '/assets/images/default_image.png';
-            const destino = producto.property_destino.join(', ') || '';
-            const tipo_actividad = producto.property_tipo.join(', ') || '';
+            const destino = producto.property_destino?.join(', ') || '-';
+            const price = producto.property_tarifa_en_doble || '';
+            const tipo_actividad = producto.property_tipo.join(', ') || '-';
             
             const id = producto.id;
 
@@ -129,52 +107,75 @@ function filterProducts() {
 document.getElementById('search').addEventListener('input', filterProducts);
 
 
-function enviarProducto(idProducto) {
+
+// Función para mostrar el modal
+function mostrarModal(mensaje) {
+    const modal = document.getElementById("resultadoModal");
+    const mensajeModal = document.getElementById("mensajeModal");
+    mensajeModal.textContent = mensaje;
+    modal.style.display = "block";
+}
+
+// Cerrar el modal al hacer clic en la "X"
+const span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    document.getElementById("resultadoModal").style.display = "none";
+}
+
+// Cerrar el modal si el usuario hace clic fuera de él
+window.onclick = function(event) {
+    const modal = document.getElementById("resultadoModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+async function enviarProducto(idProducto) {
     // Obtener el producto correspondiente  
     const productoSeleccionado = productos.find(producto => producto.id === idProducto);
-    console.log(productoSeleccionado);
-    
 
     if (!productoSeleccionado) {
-        console.error('Producto no encontrado');
+        mostrarModal('Error al enviar el producto');
         return;
     }
     
-    console.log("Id conversacion: " + eventData.data.conversation.id);
     const id_conversacion = eventData.data.conversation.id;
-    // const id_conversacion = '19666';
-    
+    const urlImagen = productoSeleccionado.property_url_foto_destacada || '../assets/images/default_image.png'; // Asegúrate de que este campo tenga la URL correcta
 
-    // Aquí puedes agregar la información que llega con el evento inicial
-    const data = {
-        
-        // ...productoSeleccionado,
-        // Agregar cualquier otra información necesaria
-        "content": `
+    try {
+        //Obtener la imagen como Blob
+        const responseImagen = await fetch(urlImagen);
+        if (!responseImagen.ok) {
+            mostrarModal('Error al enviar el producto');
+            throw new Error('Error al descargar la imagen');
+        }
+
+        const blob = await responseImagen.blob();
+
+        //Crear un FormData y agregar el archivo Blob
+        const formData = new FormData();
+        formData.append('attachments[]', blob, 'imagen2.jpg'); // Puedes ajustar el nombre del archivo según sea necesario
+
+        // Agregar el contenido del mensaje
+        formData.append('content', `
             *${productoSeleccionado.name}*
-            \n${productoSeleccionado.property_descripci_n}
-            \nPrecio: ${productoSeleccionado.property_tarifa_en_doble} US$
-            \nTipo: ${productoSeleccionado.property_tipo.join(', ')}
-            \nDestino: ${productoSeleccionado.property_destino.join(', ')}`,
-        "message_type": "outgoing"
-    };
+            \n${productoSeleccionado.property_descripci_n || 'Sin descripción'}
+            \nTipo: ${productoSeleccionado.property_tipo.join(', ') || '-'}
+            \nDestino: ${productoSeleccionado.property_destino?.join(', ') || '-'}`);
 
-    // Realizar la solicitud POST
-    fetch(`https://web.chatia.app/api/v1/accounts/11/conversations/${id_conversacion}/messages`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'api_access_token': process.env.API_ACCESS_TOKEN
-            
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(responseData => {
-        console.log('Producto enviado con éxito:', responseData);
-        // Aquí puedes manejar la respuesta de tu servidor
-    })
-    .catch(error => {
-        console.error('Error al enviar el producto:', error);
-    });
+        //Enviar la solicitud POST
+        const responsePost = await fetch(`https://web.chatia.app/api/v1/accounts/11/conversations/${id_conversacion}/messages`, {
+            method: 'POST',
+            headers: {
+                'api_access_token': 'Zpzr1UXYh3CE6eah4ZGYyc86'
+            },
+            body: formData
+        });
+
+        const responseData = await responsePost.json();
+        mostrarModal('Producto enviado con éxito');
+
+    } catch (error) {
+        mostrarModal('Error al enviar el producto');
+    }
 }
