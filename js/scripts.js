9b380931f4a885a745325988e9e2ff958443d62e
Mauricio <mauroalternativo519@gmail.com>
2024-09-05 15:10:47 -0500

    Add hoteles

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // require('dotenv').config();
 
 let productos = []; // Variable global para almacenar los productos
+let hoteles = [];
 let eventData ={};
 
 // Función para validar si el JSON es correcto
@@ -12,6 +13,7 @@ function isJSONValid(str) {
         return false;
     }
 }
+console.log("Antes del evento");
 
 // Escuchar eventos del objeto Window
 window.addEventListener("message", function (event) {
@@ -20,28 +22,84 @@ window.addEventListener("message", function (event) {
         return;
     }
 
+    console.log("Data recibida", eventData);
+    
     // Procesar los datos del evento
     eventData = JSON.parse(event.data);
+    cargarActividades();
 
 
-    // Enviar los datos a n8n y esperar la respuesta
-    fetch('https://n8n.weppa.co/webhook/notion', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(eventData)
-    })
-    .then(response => response.json())
-    .then(n8nResponse => {
-        // Verifica si el campo result contiene el array de objetos
-        productos = n8nResponse || [];
-        mostrarProductos(productos); // Mostrar todos los productos inicialmente
-    })
-    .catch((error) => {
-        document.getElementById('loading').style.display = 'none';
-    });
 });
+
+// Función para cargar las actividades (por defecto)
+function cargarActividades() {
+
+    document.getElementById('titulo-productos').innerText = 'Actividades';
+    document.getElementById('loading').style.display = 'block';
+
+    if(productos == '') {
+        
+
+        // Enviar los datos a n8n y esperar la respuesta de las actividades
+        fetch('https://n8n.weppa.co/webhook/notion-actividades', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(eventData)
+        })
+        .then(response => response.json())
+        .then(n8nResponse => {
+            console.log("Data de n8n: ", n8nResponse);
+            productos = n8nResponse || [];
+            console.log("Productos recibidos: ", productos);
+            mostrarProductos(productos);
+        })
+        .catch((error) => {
+            document.getElementById('loading').style.display = 'none';
+        });
+    }
+    console.log("Producots cargados: ", productos);
+    
+    mostrarProductos(productos);
+}
+
+
+// Función para cargar los hoteles
+function cargarHoteles() {
+    console.log("Cargando hoteles");
+    
+    document.getElementById('titulo-productos').innerText = 'Hoteles';
+    document.getElementById('loading').style.display = 'block';
+
+    if (hoteles == '') {
+
+        let html = '';
+        html += '<p>Aqui van los hoteles</p>';
+    
+        // Enviar los datos a otra URL que devuelva hoteles
+        fetch('https://n8n.weppa.co/webhook/notion-hoteles', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(eventData) // Puedes ajustar esto según lo que necesite la API de hoteles
+        })
+        .then(response => response.json())
+        .then(hotelesResponse => {
+            hoteles = hotelesResponse || [];
+            console.log("Hoteles carghados", hoteles);
+            
+            mostrarHoteles(hoteles);
+        })
+        .catch((error) => {
+            document.getElementById('loading').style.display = 'none';
+        });
+    }
+
+    mostrarHoteles(hoteles);
+
+}
 
 
 
@@ -93,6 +151,53 @@ function mostrarProductos(productos) {
 }
 
 
+// Función para mostrar los productos
+function mostrarHoteles(hoteles) {
+    let html = '';
+
+    // Si hoteles no es un array, conviértelo en un array
+    if (!Array.isArray(hoteles)) {
+        hoteles = [hoteles];
+    }
+
+    if (hoteles.length === 0) {
+        html += '<p>No hay resultados para la búsquedaaa.</p>';
+    } else {
+        hoteles.forEach(producto => {
+            const name = producto.name || 'Sin título';
+            // const description = producto.property_descripci_n || 'Sin descripción';
+            const imageUrl = producto.property_fotos[0] || '/assets/images/default_image.png';
+            const destino = producto.property_ || '-';
+            // const price = producto.property_tarifa_en_doble || '';
+            // const tipo_actividad = producto.property_tipo.join(', ') || '-';
+            
+            const id = producto.id;
+
+            
+            html += `
+                <div class="card">
+                    <div class="card-text">
+                        <h2>${name}</h2>
+        
+                        <p>Destino: ${destino}</p>
+                        
+                        
+                        <button onclick="enviarProducto('${id}')">Enviar</button>
+                    </div>
+                    <div class="card-image">
+                        <img src=   "${imageUrl}" alt="${name}">
+                    </div>
+                </div>
+            `;
+        });
+    }
+
+    document.getElementById('resumen').innerHTML = html;
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('resumen').style.display = 'flex';
+    
+}
+
 // Función para filtrar los productos por nombre
 function filterProducts() {
     const query = document.getElementById('search').value.toLowerCase();
@@ -139,7 +244,8 @@ async function enviarProducto(idProducto) {
         return;
     }
     
-    const id_conversacion = eventData.data.conversation.id;
+    // const id_conversacion = eventData.data.conversation.id;
+    const id_conversacion = '19666';
     const urlImagen = productoSeleccionado.property_url_foto_destacada || '../assets/images/default_image.png'; // Asegúrate de que este campo tenga la URL correcta
 
 
@@ -159,6 +265,7 @@ async function enviarProducto(idProducto) {
         
 
         const blob = await responseImagen.blob();
+        // const blob = productoSeleccionado.property_url_foto_destacada;
 
         //Crear un FormData y agregar el archivo Blob
         const formData = new FormData();
