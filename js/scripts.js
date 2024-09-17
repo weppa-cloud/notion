63a3681f1429fa959b62e33984cb8298e1a38c22
Mauricio <mauroalternativo519@gmail.com>
2024-09-17 17:40:34 -0500

    Add: enviar pdf, enviar fotos hotel, mostrar numero de telefono en proveedores

--- js/scripts.js
+++ js/scripts.js
@@ -65,7 +65,7 @@ function cargarActividades() {
         // Ocultar el mensaje de carga si ya hay productos cargados
         document.getElementById('loading').style.display = 'none';
         mostrarProductos(productos);
-    }
+    } 
 }
 
 
@@ -185,7 +185,10 @@ function mostrarProductos(productos) {
                         <p>Destino: ${destino}</p>
                         <p>Tipo: ${tipo_actividad}</p>
                         <div class="price">${price} US$</div>
-                        <button onclick="enviarProducto('${id}')">Enviar</button>
+                        <div class="button-container-info ">
+                            <button onclick="enviarProducto('${id}')">Enviar información</button>
+                            <button onclick="enviarPdf('${id}')">Enviar PDF</button>
+                        </div> 
                     </div>
                     <div class="card-image">
                         <img src=   "${imageUrl}" alt="${name}">
@@ -226,8 +229,10 @@ function mostrarHoteles(hoteles) {
                 <div class="card">
                     <div class="card-text">
                         <h2>${name}</h2>       
-                        <p>Destino: ${destino}</p>                      
-                        <button onclick="enviarHotel('${id}')">Enviar</button>
+                        <p>Destino: ${destino}</p>
+                        <div class="button-container-info ">                      
+                            <button onclick="enviarHotel('${id}')">Enviar información</button>
+                        </div>
                     </div>
                     <div class="card-image">
                         <img src=   "${imageUrl}" alt="${name}">
@@ -269,8 +274,11 @@ function mostrarProveedores(proveedores) {
                     <div class="card-text">
                         <h2>${name}</h2>       
                         <p>${descripcion}</p>                      
-                        <p>Ciudad: ${ciudad}</p>                      
-                        <button onclick="window.open('https://api.whatsapp.com/send?phone=${telefono.replace(/\s+/g, '')}', '_blank')">Contactar</button>
+                        <p>Ciudad: ${ciudad}</p>
+                        <p>Contacto: ${telefono.replace(/\s+/g, '')}</p>
+                        <div class="button-container-info ">                      
+                            <button onclick="window.open('https://api.whatsapp.com/send?phone=${telefono.replace(/\s+/g, '')}', '_blank')">Contactar</button>
+                        </div>
                     </div>
                 </div>
             `;
@@ -318,7 +326,7 @@ function filterProducts() {
 // Función para limpiar el cuadro de búsqueda
 function limpiarBusqueda() {
     document.getElementById('search').value = ''; // Limpiar el campo de búsqueda
-  }
+}
 
 // Añadir event listener al cuadro de búsqueda
 document.getElementById('search').addEventListener('input', filterProducts);
@@ -437,12 +445,12 @@ async function enviarHotel(idProducto) {
     const productoSeleccionado = hoteles.find(producto => producto.id === idProducto);
 
     if (!productoSeleccionado) {
-        mostrarModal('Error al enviar el producto - Producto seleccionado no existe');
+        mostrarModal('Error al enviar el hotel');
         return;
     }
 
     // Mostrar el modal de "Enviando..." y deshabilitar el cierre
-    mostrarModal('Enviando producto...', true); // El segundo argumento true deshabilita el cierre
+    mostrarModal('Enviando Hotel...', true); // El segundo argumento true deshabilita el cierre
     
     // const id_conversacion = eventData.data.conversation.id;
     const id_conversacion = '19666';
@@ -450,27 +458,12 @@ async function enviarHotel(idProducto) {
     const urlPage = productoSeleccionado.url;
     const name = productoSeleccionado.name.trim();
     const destino = productoSeleccionado.property_destinos;
-    
+    const cantidad = productoSeleccionado.property_fotos.length;
+
     try {      
-        const responseImagen = await fetch('https://n8n.weppa.co/webhook/image-hotel', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name: name }),
-        });
         
-        if (!responseImagen.ok) {
-            mostrarModal('Error al obtener el producto en responseImagen');
-            throw new Error('Error al obtener el producto en responseImagen');
-        }
-        
-
-        const blob = await responseImagen.blob();
-
         //Crear un FormData y agregar el archivo Blob
         const formData = new FormData();
-        formData.append('attachments[]', blob, 'imagen.jpg'); // Puedes ajustar el nombre del archivo según sea necesario
 
         // Agregar el contenido del mensaje
         formData.append('content', `
@@ -487,10 +480,122 @@ async function enviarHotel(idProducto) {
             body: formData
         });
         
-        const responseData = await responsePost.json();
-        mostrarModal('Producto enviado con éxito');
+        // const responseData = await responsePost.json();
+        // mostrarModal('Producto enviado con éxito');
 
     } catch (error) {        
-        mostrarModal('Error al enviar el producto General');
+        mostrarModal('Error al enviar el hotel');
+    }
+
+
+    // Enviar fotos
+    try {    
+        for (let i = 0; i < cantidad; i++) {  
+        const responseImagen = await fetch('https://n8n.weppa.co/webhook/image-hotel', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                name: name,
+                nameFoto: `Fotos_${i}`
+                }),
+        });
+        
+        if (!responseImagen.ok) {
+            mostrarModal('Error al obtener fotos');
+            throw new Error('Error al obtener fotos');
+        }
+
+        const blob = await responseImagen.blob();
+        
+        //Crear un FormData y agregar el archivo Blob
+        const formData = new FormData();
+        formData.append('attachments[]', blob, `Foto_${i+1}`); // Puedes ajustar el nombre del archivo según sea necesario
+
+        // Agregar el contenido del mensaje
+        formData.append('content', ``);
+        
+
+        //Enviar la solicitud POST
+        const responsePost = await fetch(`https://web.chatia.app/api/v1/accounts/11/conversations/${id_conversacion}/messages`, {
+            method: 'POST',
+            headers: {
+                'api_access_token': 'Zpzr1UXYh3CE6eah4ZGYyc86'
+            },
+            body: formData
+        });
+        
+        const responseData = await responsePost.json();
+        
+    }
+        mostrarModal('Hotel enviado con éxito');
+
+    } catch (error) {        
+        mostrarModal('Error al enviar hotel');
+    }
+    
+    
+}
+
+
+async function enviarPdf(idProducto) {
+    // Obtener el producto correspondiente  
+    const productoSeleccionado = productos.find(producto => producto.id === idProducto);
+
+    if (!productoSeleccionado) {
+        mostrarModal('Error al enviar el archivo PDF');
+        return;
+    }
+
+    // Mostrar el modal de "Enviando..." y deshabilitar el cierre
+    mostrarModal('Enviando archivo PDF...', true); // El segundo argumento true deshabilita el cierre
+
+    // const id_conversacion = eventData.data.conversation.id;
+    const id_conversacion = '19666';
+    // const urlImagen = productoSeleccionado.property_url_foto_destacada || '../assets/images/default_image.png';
+    const name = productoSeleccionado.name;
+
+    try {
+        // Enviar la URL de la imagen al webhook de n8n
+        const responseImagen = await fetch('https://n8n.weppa.co/webhook/pdf-actividad', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name: name }),
+        });
+
+        if (!responseImagen.ok) {
+            mostrarModal('Error al obtener el archivo PDF', false);
+            throw new Error('Error al obtener el archivo PDF');
+        }
+
+        const blob = await responseImagen.blob();
+
+        // Crear un FormData y agregar el archivo Blob
+        const formData = new FormData();
+        formData.append('attachments[]', blob, `${name}.pdf`);
+
+        // Agregar el contenido del mensaje
+        formData.append('content', ``);
+
+        // Enviar la solicitud POST a Chatia
+        const responsePost = await fetch(`https://web.chatia.app/api/v1/accounts/11/conversations/${id_conversacion}/messages`, {
+            method: 'POST',
+            headers: {
+                'api_access_token': 'Zpzr1UXYh3CE6eah4ZGYyc86'
+            },
+            body: formData
+        });
+
+        const responseData = await responsePost.json();
+   
+        // Mostrar mensaje de éxito
+        mostrarModal('PDF enviado con éxito', false);
+
+    } catch (error) {
+        // Mostrar mensaje de error
+        mostrarModal('Error al enviar el PDF', false);
     }
 }
